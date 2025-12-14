a970b7a4a5d86fb8591f73519187ef08eafc6064
binayakbhandari <binayakbhandari60@gmail.com>
2025-12-14 07:49:45 +0545

    Updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,14 +24,20 @@ const Form = ({type,id}) => {
         const { name, value } = e.target
         setBook({
             ...book,
-            [name] : value
+            [name] : name === "bookImage" ? e.target.files[0] : value
         })
     }
-
+    console.log(book)
     const createBook = async (e) => {
         e.preventDefault()
         if (type === "Create") {
-            const response = await axios.post("http://localhost:3000/book", book)
+            const response = await axios.post("http://localhost:3000/book", book, 
+                {
+                    headers : {
+                        "Content-Type" : "multipart/form-data"
+                    }
+                }
+            )
             if (response.status === 200) {
                 navigate("/")
             } else {
@@ -39,7 +45,13 @@ const Form = ({type,id}) => {
             }
         } 
         else {
-            const response = await axios.patch(`http://localhost:3000/book/${id}`, book)
+            const response = await axios.patch(`http://localhost:3000/book/${id}`, book,
+                {
+                    headers : {
+                        "Content-Type" : "multipart/form-data"
+                    }
+                }
+            )
             if (response.status === 200) {
                 navigate(`/book/${id}`)
             } else {
@@ -98,9 +110,9 @@ const Form = ({type,id}) => {
                                             <path d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clipRule="evenodd" fillRule="evenodd" />
                                         </svg>
                                         <div className="mt-4 flex text-sm/6 text-gray-600">
-                                            <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500">
+                                            <label htmlFor="bookImage" className="relative cursor-pointer rounded-md bg-transparent font-semibold text-indigo-600 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 hover:text-indigo-500">
                                                 <span>Upload a file</span>
-                                                <input onChange={handleChange} id="file-upload" type="file" name="bookImage" className="sr-only" />
+                                                <input onChange={handleChange} id="bookImage" type="file" name="bookImage" className="sr-only" />
                                             </label>
                                             <p className="pl-1">or drag and drop</p>
                                         </div>
