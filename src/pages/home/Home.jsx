375cf908031db02105f33ba9de19cd75b2623c9b
binayakbhandari <binayakbhandari60@gmail.com>
2025-12-20 10:41:44 +0545

    Project Online

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,92 @@
-import { Link } from "react-router-dom"
-import Card from "../../components/Card"
-import Navbar from "../../components/Navbar"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import Floating404 from "../../components/Floating404"
+// import { Link } from "react-router-dom"
+// import Card from "../../components/Card"
+// import Navbar from "../../components/Navbar"
+// import axios from "axios"
+// import { useEffect, useState } from "react"
+// import Floating404 from "../../components/Floating404"
+
+// const Home = () => {
+//     const [books, setBooks] = useState([])
+//     const fetchBooks = async () => {
+//         // Fetch book logic here
+//         const response = await axios.get('https://nodejs-crud-practice.onrender.com/book')
+//         if (response.status === 200) {
+//             // setBooks(response.data)
+//             setBooks(response.data.data || [])
+//         }
+//     }
+
+//     useEffect(() => {
+//         fetchBooks()
+//     }, [])
+
+//     return (
+//         <>
+//             <Navbar />
+//             <div className="flex flex-wrap justify-center gap-10 mt-10 mb-10">
+//                 {books.length === 0 ? (
+//                     <Floating404 />
+//                 ) : (
+//                     books.map((book) => (
+//                         <Link to={`/book/${book._id}`} key={book._id}>
+//                             <Card book={book} />
+//                         </Link>
+//                     ))
+//                 )}
+//             </div>
+
+//         </>
+//     )
+// }
+
+// export default Home
+
+import { Link } from "react-router-dom";
+import Card from "../../components/Card";
+import Navbar from "../../components/Navbar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Floating404 from "../../components/Floating404";
 
 const Home = () => {
-    const [books, setBooks] = useState([])
-    const fetchBooks = async () => {
-        // Fetch book logic here
-        const response = await axios.get('https://nodejs-crud-practice.onrender.com/book')
-        if (response.status === 200) {
-            // setBooks(response.data)
-            setBooks(response.data.data || [])
-        }
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get(
+        "https://nodejs-crud-practice.onrender.com/book"
+      );
+      if (response.status === 200) {
+        setBooks(response.data.data || []);
+      }
+    } catch (error) {
+      console.error("Error fetching books:", error);
+      setBooks([]);
     }
+  };
 
-    useEffect(() => {
-        fetchBooks()
-    }, [])
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-wrap justify-center gap-10 mt-10 mb-10">
-                {books.length === 0 ? (
-                    <Floating404 />
-                ) : (
-                    books.map((book) => (
-                        <Link to={`/book/${book._id}`} key={book._id}>
-                            <Card book={book} />
-                        </Link>
-                    ))
-                )}
-            </div>
+  return (
+    <>
+      <Navbar />
+      <div className="px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
+        <div className="flex flex-wrap justify-center gap-x-6 gap-y-8">
+          {books.length === 0 ? (
+            <Floating404 />
+          ) : (
+            books.map((book) => (
+              <Link to={`/book/${book._id}`} key={book._id}>
+                <Card book={book} />
+              </Link>
+            ))
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Home
+export default Home;
